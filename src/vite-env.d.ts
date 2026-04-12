7c2d208ea2c418f26d585ee31c2a1a126bc96657
biserkan <biserkanla@gmail.com>
2026-04-12 22:14:59 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/yazili/', // GitHub'daki depo (repository) adınızı buraya tam olarak yazın
+})
