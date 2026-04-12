8a727e8324c9b58db2c04190e6a53dd7667ce543
biserkan <biserkanla@gmail.com>
2026-04-12 23:24:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yazili/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
