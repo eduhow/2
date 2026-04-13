b291b5536e9b721c90c97c7773a581edb69452c0
eduhow <biserkanla@gmail.com>
2026-04-13 17:20:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/yazili/',
+  base: '/2/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
