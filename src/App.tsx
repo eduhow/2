1da69d93e4bb5cbc0a2cd5f298c3c2416137d413
eduhow <biserkanla@gmail.com>
2026-04-14 13:21:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import A4Template from "@/components/A4Template";
 
 function App() {
-  // 1. Vite'ı kandırmak ve derleme boyutunu değiştirmek için bir log ekliyoruz
-  console.log("GitHub Actions Güncelleme Testi - Sürüm 2.0");
-
   return (
-    <>
-      {/* 2. Ekranda görünmeyen ama HTML yapısını değiştiren bir element ekliyoruz */}
-      <div style={{ display: 'none', position: 'absolute' }}>
-        Sistem Tetikleme V2.0 - {new Date().toISOString()}
-      </div>
-      
-      <A4Template />
-    </>
+	<main className="min-h-screen w-full flex flex-col">
+ 	 <A4Template />
+	</main>
   );
 }
-
-export default App;
